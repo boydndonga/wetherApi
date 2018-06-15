b88d84fa27fdfb82e384169f937c67ca9caec698
Boyd Ndonga <boydbugle@gmail.com>
2018-06-15 15:27:06 +0300

    compile ButterKnife into weatherApiu dependencies

--- app/src/main/java/com/example/ndonga/weatherapi/MainActivity.java
+++ app/src/main/java/com/example/ndonga/weatherapi/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ndonga.weatherapi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -10,6 +11,8 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.buttonLogin)
     Button mButtonlogin;
+    @BindView(R.id.textViewUserEmail)
+    TextView mTextViewUserEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

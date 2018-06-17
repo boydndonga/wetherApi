d215d37c5db37db84d250b14e2090b8708127113
Boyd Ndonga <boydbugle@gmail.com>
2018-06-17 14:02:04 +0300

    configure environ variables in constant file

--- app/src/main/java/com/example/ndonga/weatherapi/Constants.java
+++ app/src/main/java/com/example/ndonga/weatherapi/Constants.java
@@ -1,5 +1,7 @@
 package com.example.ndonga.weatherapi;
 
 public class Constants {
-    public static final String WEATHER_CONSUMER_KEY = BuildConfig.WEATHER_CONSUMER_KEY;
+    public static final String WEATHER_APP_ID = BuildConfig.WEATHER_APP_ID;
+    public static final String WEATHER_BASE_URL = "https://api.yelp.com/v2/search?term=food";
+    public static final String WEATHER_LOCATION_QUERY_PARAMETER = "q";
 }

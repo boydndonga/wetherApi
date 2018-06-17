9ff358e64bde409e395335e460115069992974c2
Boyd Ndonga <boydbugle@gmail.com>
2018-06-17 17:57:26 +0300

    create recyclerview

--- app/src/main/java/com/example/ndonga/weatherapi/adapters/WeatherAdapter.java
+++ app/src/main/java/com/example/ndonga/weatherapi/adapters/WeatherAdapter.java
@@ -2,17 +2,67 @@ package com.example.ndonga.weatherapi.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.ndonga.weatherapi.R;
 import com.example.ndonga.weatherapi.models.Weather;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder> {
     private List<Weather> mLocationWeather = new ArrayList<>();
     private Context mContext;
 
-    public RestaurantListAdapter(Context context, List<Weather> locationWeather) {
+    public WeatherAdapter(Context context, List<Weather> locationWeather) {
         mContext = context;
         mLocationWeather = locationWeather;
     }
+
+
+    @Override
+    public WeatherAdapter.WeatherViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.weather_list_item, parent, false);
+        WeatherViewHolder viewHolder = new WeatherViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(WeatherAdapter.WeatherViewHolder holder, int position) {
+        holder.bindWeather(mLocationWeather.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mLocationWeather.size();
+    }
+
+    public class WeatherViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.temperatureTextView)
+        TextView mTemperatureTextView;
+        @BindView(R.id.conditionTextView)
+        TextView mConditionTextView;
+        @BindView(R.id.locationTextView)
+        TextView mLocationTextView;
+
+        private Context mContext;
+
+        public WeatherViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindRestaurant(Restaurant restaurant) {
+            mNameTextView.setText(restaurant.getName());
+            mCategoryTextView.setText(restaurant.getCategories().get(0));
+            mRatingTextView.setText("Rating: " + restaurant.getRating() + "/5");
+        }
+    }
+}
--- app/src/main/java/com/example/ndonga/weatherapi/ui/WeatherActivity.java
+++ app/src/main/java/com/example/ndonga/weatherapi/ui/WeatherActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import com.example.ndonga.weatherapi.adapters.WeatherAdapter;
 import com.example.ndonga.weatherapi.models.Weather;
 import com.example.ndonga.weatherapi.services.WeatherService;
-import com.example.ndonga.weatherapi.R
+import com.example.ndonga.weatherapi.R;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -45,10 +45,9 @@ public class WeatherActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 mLocationWeather = weatherService.processResults(response);
-
-                WeatherActivity.this.runOnUiThread(() -> {
+                Weather.this.runOnUiThread(() -> {
                     mAdapter = new WeatherAdapter(getApplicationContext(), mLocationWeather);
-//
+
                     mRecyclerView.setAdapter(mAdapter);
                     RecyclerView.LayoutManager layoutManager
                             = new LinearLayoutManager(WeatherActivity.this);
